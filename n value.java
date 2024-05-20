679d18385a20d0d5ad06cbd5cc87475837364758
csa1621dwdm <132541314+vijayasudhakar@users.noreply.github.com>
2024-05-20 15:32:09 +0530

    Create n value.java

---
+++ n value.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class value
+{
+	public static void main(String ar[])
+{
+	Scanner s = new Scanner(System.in);
+	int i,n;
+	i = 0;
+	System.out.println("Enter the n value:");
+	n = s.nextInt();
+	for(i=0;i<=n;i++)
+		System.out.println(i);
+}
+}
