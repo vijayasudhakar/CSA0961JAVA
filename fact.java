89db64b385eb3dfa4ae76f1e43d087801267e737
csa1621dwdm <132541314+vijayasudhakar@users.noreply.github.com>
2024-05-20 15:35:43 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class fact
+{
+	public static void main(String ar[])
+{
+	Scanner s = new Scanner(System.in);
+	int i,n,fact;
+	fact = 1;
+	System.out.println("Enter the n value:");
+	n = s.nextInt();
+	for(i=1;i<=n;i++)
+	{
+	fact = fact*i;
+	}
+	System.out.println("Factorial of "+n+" is"+fact); 
+}
+}
