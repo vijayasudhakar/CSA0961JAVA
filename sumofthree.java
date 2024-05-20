ae44c6412122e65679fb52b3ae352a81b2ae6369
csa1621dwdm <132541314+vijayasudhakar@users.noreply.github.com>
2024-05-20 15:24:29 +0530

    Create sumofthree.java

---
+++ sumofthree.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class sumofthree
+{
+	public static void main(String arg[])
+{
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter the value of A:");
+	int a = s.nextInt();
+	System.out.println("Enter the value of B:");
+	int b = s.nextInt();
+	System.out.println("Enter the value of C:");
+	int c = s.nextInt();
+	int Sum = a + b + c;
+	System.out.println("The Sum of three Numbers:"+Sum);
+}
+}
