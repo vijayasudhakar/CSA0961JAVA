512283ed22529d905277029115cbc89c16041ec3
csa1621dwdm <132541314+vijayasudhakar@users.noreply.github.com>
2024-06-26 19:36:39 +0530

    Create P1-Pattern1.java

---
+++ P1-Pattern1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class king{
+	public static void main(String args[])
+	{
+		int i, j,n;
+		Scanner s = new Scanner(System.in);
+		n = s.nextInt();
+		for (i = 1; i <= n; i++) {
+			for (j = 1; j <= i; j++) {
+				System.out.print("1");
+			}
+			System.out.println();
+		}
+	}
+}
