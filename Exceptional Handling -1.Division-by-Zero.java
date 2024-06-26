063c120e2ede6fddfb1dc7d570c5106a23b5cbbd
csa1621dwdm <132541314+vijayasudhakar@users.noreply.github.com>
2024-06-26 19:29:26 +0530

    Create Exceptional Handling -1.Division-by-Zero.java

---
+++ Exceptional Handling -1.Division-by-Zero.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class king {
+	public static void main(String arg[])
+	{
+		int a = 5;
+		int b = 0;
+		try {
+			System.out.println(a / b); 
+		}
+		catch (Exception ex) {
+		System.out.println("Divided by zero operation cannot possible");
+		}
+	}
+}
