da2140215cb5144893ec352b28081dc331df9462
csa1621dwdm <132541314+vijayasudhakar@users.noreply.github.com>
2024-06-26 19:17:31 +0530

    Create 28.inchtom.java

---
+++ 28.inchtom.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class king
+{
+  public static void main(String a[])
+{
+	Scanner s = new Scanner(System.in);
+	Double i,n;
+	System.out.println("Enter the Inches:");
+	n = s.nextDouble();    
+	i = n*0.0254;
+	System.out.println("Meters:"+i);
+}
+}
