c6ed3ea91509b7c78f9aec2ea6425b68974d39cf
csa1621dwdm <132541314+vijayasudhakar@users.noreply.github.com>
2024-06-26 19:44:25 +0530

    Create P8-Patternstarspace.java

---
+++ P8-Patternstarspace.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class king{
+	public static void main(String args[])
+	{
+		int i, j,n;
+		Scanner s = new Scanner(System.in);
+		n = s.nextInt();
+		for (i = 1; i <= n; i++) {
+			for (j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (j = 1; j <= i; j++) {
+				System.out.print("*" + " ");
+			}
+			System.out.println();
+		}
+	}
+}
