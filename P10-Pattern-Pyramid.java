c178a9d6c179ef3a002193f0b1c1bf1c161363d7
csa1621dwdm <132541314+vijayasudhakar@users.noreply.github.com>
2024-06-26 19:37:07 +0530

    Create P10-Pattern-Pyramid.java

---
+++ P10-Pattern-Pyramid.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class king{
+public static void main(String arg[]){
+	int i,n,j;
+	Scanner s = new Scanner(System.in);
+	n = s.nextInt();
+	for(i=1;i<=n;i++){
+		for(j=1;j<=i;j++){
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+	for(i=n;i>=1;i--){
+		for(j=1;j<=i;j++){
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}}
