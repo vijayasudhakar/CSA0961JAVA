a0dacc21a5ee90a023742cd566c09a7245ab1ca3
csa1621dwdm <132541314+vijayasudhakar@users.noreply.github.com>
2024-06-29 10:37:15 +0530

    Create String1.java

---
+++ String1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class king{
+public static void main(String arg[]){
+	Scanner s = new Scanner(System.in);
+	String s1,s2;
+	s1 = s.nextLine();
+	s2 = s.nextLine();
+	if(s1.equals(s2)){
+	System.out.println("Valid");
+	}
+	else{
+	System.out.println("Invalid");
+	}
+}
+}
+	
